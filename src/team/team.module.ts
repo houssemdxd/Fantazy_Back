c7b67ef111fd6a8444927fa7318bdeb1ea177996
Houssem Khalfaoui <gameplayclip4@gmail.com>
2025-07-26 16:17:45 +0100

    fix team

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Team, TeamSchema } from './entities/team.entity';
 
 @Module({
+
+
+ imports: [
+    MongooseModule.forFeature([{ name: Team.name, schema: TeamSchema }]),
+  ],
+
   controllers: [TeamController],
   providers: [TeamService]
 })
